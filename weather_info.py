471530e2e0107ea93beabca9b1e8532a12ed775c
mcoudert2000 <mcoudertjr@gmail.com>
2021-03-29 18:37:59 +0100

    CORRECTED

--- weather_info.py
+++ weather_info.py
@@ -1,27 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 30 11:35:13 2020
-
-@author: matthewcoudert
-"""
-
 import requests
 
-weather_key = {'x-api-key':'5ojBayWjBWReRKVaecaN1Rtz92nEHYOD'}
 
-def get_weather_by_day(lat,lon,date): # Input: Latitude, Longitude, YYYY-MM-DD
-    weather_params = {'lat':lat,'lon':lon,'start':date,'end':date}
-    out = requests.get("https://api.meteostat.net/v2/point/hourly",
+weather_key = {'x-api-key':'4FdQRIr7sIZm6pvSzrBIY0hl4ln0qzRf'}
+
+def get_weather(lat,lon,date,hour): # Input: Latitude, Longitude, YY-MM-DD, Hour
+    station = get_nearby_station(lat,lon)
+    return get_wind(get_weather_by_day(station,date)['data'][hour])
+
+def get_weather_by_day(station,date): # Input: Latitude, Longitude, YYYY-MM-DD
+    weather_params = {'station' : station, 'start' : date, 'end' :date, 'model': 1}
+    out = requests.get("https://api.meteostat.net/v2/stations/hourly",
                     params = weather_params,                   
                     headers = weather_key)
     return(out.json())
 
-def get_weather(lat,lon,date,hour): # Input: Latitude, Longitude, YY-MM-DD, Hour
-    return get_weather_by_day(lat,lon,date)['data'][hour]
-
 def get_wind(weather):
-    wind_dict = {'time':weather['time'],'wind_speed':weather['wspd'],'wind_direction':weather['wdir']}
+    wind_dict = {'time':weather['time'],'wind_speed':weather['wspd'],'wind_direction':weather['wdir'],'temp':weather['temp']}
     return wind_dict
 
-test_weather = get_weather(52.4118,1.7776,'1999-11-03',9)
+def get_nearby_station(lat,lon):
+    params = {'lat':lat, 'lon':lon}
+    out = requests.get('https://api.meteostat.net/v2/stations/nearby',
+                       params = params,
+                       headers = weather_key).json()['data'][0]['id']
+    return out
